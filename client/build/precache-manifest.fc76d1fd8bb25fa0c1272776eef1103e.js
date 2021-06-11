8a9cc220d2252a42cc411b528c81d7dafeba14ab
unknown <andriyanatanasov@gmail.com>
2021-06-11 18:10:52 +0300

    message

---
+++ client/build/precache-manifest.fc76d1fd8bb25fa0c1272776eef1103e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e008c9c36fc129da12e3932652f3e4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95016a4498078457338c",
+    "url": "/static/css/main.9c209bf4.chunk.css"
+  },
+  {
+    "revision": "76e29d5ae6603d88afb7",
+    "url": "/static/js/2.f2cae53a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2cae53a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95016a4498078457338c",
+    "url": "/static/js/main.6e62f519.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "3a216a1b1024d9e75eacd84ee06e9f6a",
+    "url": "/static/media/UB.3a216a1b.jpg"
+  }
+]);
